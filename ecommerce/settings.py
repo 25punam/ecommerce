521438e093f6a3145279d9d85cc7c6c03b9feb0c
Base25Punam <25punamgode@gmail.com>
2025-04-06 19:37:59 +0530

    Initial commit

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -80,11 +80,21 @@ TEMPLATES = [
 WSGI_APPLICATION = "ecommerce.wsgi.application"
 
 # Database
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         #'NAME': BASE_DIR / 'db.sqlite3',
+#         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#     }
+# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        #'NAME': BASE_DIR / 'db.sqlite3',
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydb',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': 'localhost', 
+        'PORT': '3306',       
     }
 }
 
