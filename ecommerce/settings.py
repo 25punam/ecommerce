ac47b1d560f0c0e28793e996bc1c4323635cdc99
Base25Punam <25punamgode@gmail.com>
2026-04-21 19:31:22 +0530

    fix BASE_DIR and media path

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -13,16 +13,12 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 TEMPLATES_DIR = BASE_DIR / "templates"
 
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
-
+MEDIA_ROOT = BASE_DIR / "media"
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-$8^=p0ib)98)$d!0)d)rs*qkkg$ug8awkf6_wm(ypd!qz$z9=)"
